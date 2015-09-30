f2a1fe76d46c1f328f4bf46976637cf86cbd7f92
mumrau <mc.mumrau@gmail.com>
2015-09-30 14:16:32 +0200

    Update 1 Test

--- src/src/manipulationImage/Lsb.java
+++ src/src/manipulationImage/Lsb.java
@@ -6,14 +6,16 @@ import image.NotreRepresentationImage;
  * Created by Flavian Jacquot on 30/09/2015.
  */
 public class Lsb {
+
     //TODO stub
-    public String putTextIn(String text, byte[] bytes)
+    public byte[] putDataIn(byte[] toHide, byte[] wrapper)
     {
         for (int i=7; i<text.size(); i=+8){
-            bytes[i]=0;//premier bit du string etc etc
+            bytes[i]=0;
         }
-    return text;
+    return wrapper;
     }
+
     public String getTextIn(byte[] bytes)
     {
         return "";
