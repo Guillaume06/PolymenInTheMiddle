5b636099bf976ba53261eef6ee516632aeb8b252
mumrau <mc.mumrau@gmail.com>
2015-09-30 14:09:49 +0200

    Update 1

--- src/src/manipulationImage/Lsb.java
+++ src/src/manipulationImage/Lsb.java
@@ -9,6 +9,9 @@ public class Lsb {
     //TODO stub
     public String putTextIn(String text, byte[] bytes)
     {
+        for (int i=7; i<text.size(); i=+8){
+            bytes[i]=0;//premier bit du string etc etc
+        }
     return text;
     }
     public String getTextIn(byte[] bytes)
