a54362fe734664c7031c7056a7232949e2289c4e
mumrau <mc.mumrau@gmail.com>
2015-09-30 17:10:36 +0200

    Lsb supposed to work

--- src/src/manipulationImage/Lsb.java
+++ src/src/manipulationImage/Lsb.java
@@ -4,19 +4,17 @@ package manipulationImage;
 
 /**
  * @author Mathias Chevalier
- * @school Polytech' Nice
- * @created 30/09/2015.
  */
 public class Lsb {
 
     //TODO stub
-    public byte[] putDataIn(byte[] toHide, byte[] wrapper)
+    public static byte[] putDataIn(byte[] toHide, byte[] wrapper)
     {
         byte[] res = new byte[wrapper.length];
         int j=0;
         //parcours du tableau d'octets à cacher (poids fort vers poids faible)
         for (byte tmp : toHide){
-            for (int i=8; i<=1; i++){
+            for (int i=8; i>=1; i++){
                 int value = getBit(i, tmp);
                 res[j]=setBit(1,value,wrapper[j]);
                 j++;
@@ -25,14 +23,15 @@ public class Lsb {
         }
     return res;
     }
-
-    /*public String getDataIn(byte[] bytes) {
-        int j=0;
+    //méthode pour récuperer des données codées sur chacun des bits de poids faible d'un tableau d'octets
+    public static String getDataIn(byte[] bytes) {
+        String toReturn = "";
         for (byte tmp : bytes){
-            for (int i=1)
-        }
-        return "";
-    }*/
+            int value = getBit(1, tmp);
+            toReturn += Integer.toString(value);
+            }
+        return toReturn;
+}
 
     //méthode pour récupérer un bit à la position x dans un byte
     public static int getBit(int position, byte octet){
