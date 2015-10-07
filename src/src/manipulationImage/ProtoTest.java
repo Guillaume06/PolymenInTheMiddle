f99afa4a90ee74b80d81492d198734268e5bc4e7
Guillaume06 <guillaume_fdr@orange.fr>
2015-10-07 16:09:18 +0200

    Ajout du code C

---
+++ src/src/manipulationImage/ProtoTest.java
@@ -0,0 +1,40 @@
+package manipulationImage;
+/**
+ * Created by Guillaume on 30/09/2015.
+ */
+public class ProtoTest {
+    public static void rotate(String fileName, double rotation){
+        Picture pic1 = new Picture(fileName);
+        pic1.show();
+        int width  = pic1.width();
+        int height = pic1.height();
+
+        double angle = Math.toRadians(rotation);
+        double sin = Math.sin(angle);
+        double cos = Math.cos(angle);
+        double x0 = 0.5 * (width  - 1);     // point to rotate about
+        double y0 = 0.5 * (height - 1);     // center of image
+
+        Picture pic2 = new Picture(width, height);
+
+        // rotation
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                double a = x - x0;
+                double b = y - y0;
+                int xx = (int) (+a * cos - b * sin + x0);
+                int yy = (int) (+a * sin + b * cos + y0);
+
+                // plot pixel (x, y) the same color as (xx, yy) if it's in bounds
+                if (xx >= 0 && xx < width && yy >= 0 && yy < height) {
+                    pic2.set(x, y, pic1.get(xx, yy));
+                }
+            }
+        }
+
+        pic2.show();
+    }
+    public static void main(String args[]){
+        rotate("google-art.jpg", 45);
+    }
+}
